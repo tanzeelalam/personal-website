fa0a11377780b1cb94ef14d50b6ceea8e5ecf9c0
Tanzeel Alam <tanzeelalam@gmail.com>
2026-06-11 16:25:05 +0530

    Add domain changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -72,9 +72,16 @@ resource "aws_cloudfront_distribution" "website" {
     }
   }
 
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
+ viewer_certificate {
+  acm_certificate_arn      = aws_acm_certificate_validation.website.certificate_arn
+  ssl_support_method       = "sni-only"
+  minimum_protocol_version = "TLSv1.2_2021"
+}
+
+  aliases = [
+  "tanzeelalam.co.in",
+  "www.tanzeelalam.co.in"
+]
 }
 
 data "aws_iam_policy_document" "cloudfront_access" {
@@ -112,4 +119,78 @@ resource "aws_s3_bucket_policy" "website" {
 
 output "cloudfront_url" {
   value = aws_cloudfront_distribution.website.domain_name
+}
+
+data "aws_route53_zone" "website" {
+  name         = "tanzeelalam.co.in"
+  private_zone = false
+}
+
+resource "aws_acm_certificate" "website" {
+  provider = aws.virginia
+
+  domain_name               = "tanzeelalam.co.in"
+  subject_alternative_names = ["www.tanzeelalam.co.in"]
+
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.website.domain_validation_options :
+    dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  zone_id         = data.aws_route53_zone.website.zone_id
+
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+}
+
+resource "aws_acm_certificate_validation" "website" {
+  provider = aws.virginia
+
+  certificate_arn = aws_acm_certificate.website.arn
+
+  validation_record_fqdns = [
+    for record in aws_route53_record.cert_validation :
+    record.fqdn
+  ]
+}
+
+resource "aws_route53_record" "root" {
+  zone_id = data.aws_route53_zone.website.zone_id
+
+  name = "tanzeelalam.co.in"
+  type = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.website.domain_name
+    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.website.zone_id
+
+  name = "www"
+  type = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.website.domain_name
+    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
+    evaluate_target_health = false
+  }
 }
